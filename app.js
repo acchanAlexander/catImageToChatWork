5b3b6632f24b45ca2392f194044ed9a4f8365651
acchanAlexander <AtariYoshihiro@next-group.jp>
2016-06-13 00:22:54 +0900

    画像リンクをチャットワークに投稿

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 const exec = require('child_process').exec
     , fs = require('fs')
-    , fetchCatImageCmd = "curl -k 'https://www.googleapis.com/customsearch/v1?key=" + process.env.GOOGLE_CUSTOMSEARCH_APIKEY + "&cx=" + process.env.CSE_ID + "&searchType=image&q=neko'"
+    , fetchCatImageCmd = "curl -k 'https://www.googleapis.com/customsearch/v1?key=" + process.env.GOOGLE_CUSTOMSEARCH_APIKEY + "&cx=" + process.env.CSE_ID + "&searchType=image&q=ねこ'"
     ;
 
 exec(fetchCatImageCmd,
@@ -17,14 +17,30 @@ exec(fetchCatImageCmd,
     const searchResult = JSON.parse(stdout)
         , catImagesInfo = searchResult.items
         , rand = Math.floor(Math.random() * 10)
-        , catImage = catImagesInfo[rand]
+        , catImageLink = catImagesInfo[rand].link
         ;
 
-    // debug
-    console.log(rand);
-    console.log(catImage);
+    postChatWork(catImageLink);
   }
 );
+
+function postChatWork(imageLink) {
+  const msg = 'にゃんこ画像でも見て一息つきましょう(=^･ω･^=)\n' + imageLink
+      , cmdPostChatWork = 'curl -X POST -H "X-ChatWorkToken: ' + process.env.CHATWORK_TOKEN + '" -d "body=' + msg + '" "https://api.chatwork.com/v1/rooms/' + process.env.CHATWORK_CAT_ROOM_ID + '/messages"'
+      ;
+
+  exec(cmdPostChatWork,
+    function (err, stdout, stderr) {
+      if (err){
+        writeLog('err.log', err);
+        return;
+      }
+
+      writeLog('stdout.log', stdout);
+      writeLog('stderr.log', stderr);
+    }
+  );
+};
 
 function writeLog(filename, data) {
   fs.writeFile(filename, data, (err) => {
